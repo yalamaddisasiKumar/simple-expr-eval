8c60e0f358f4085c6714aa4b7b7868c93cfff0c7
sasikumar yalamaddi <ysasikumar12345@gmail.com>
2022-07-28 17:23:03 +0530

    initial code setup

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export declare function evaluate(expression: string): number | Error;
---
+++ src/index.ts
@@ -0,0 +1,428 @@
+
+enum Token{
+    Add = 1,
+    Sub,
+    Multi,
+    Divide,
+    Caret,
+    LeftPerm,
+    RightPerm,
+    Num,
+    // EOF,
+}
+
+enum TokenValue{
+    Pluse = '+',
+    Minus = '-',
+    Star = '*',
+    ForwardSlash = '/',
+    Caret = '^',
+    LeftPerm = '(',
+    RightPerm = ')'
+}
+
+// enum OperPres{
+//     DefaultZero,
+//     AddSub,
+//     MulDiv,
+//     Power,
+//     Negative,
+//     Perm
+// }
+
+// type TokenObj = {
+//     token: Token.Num,
+//     data:  number,
+// } | {
+//     token: Token,
+//     data?:  number,
+// }
+
+
+
+type TokenObj = {
+    token: Exclude<Token, Token.Num> ,
+    data?: number,
+} | {
+    token: Token.Num,
+    data:  number,
+}
+
+// function getTokenPrec(tObj: TokenObj): OperPres{
+//     switch(tObj.token){
+//         case Token.Add : {
+//             return OperPres.AddSub
+//         };
+//         case Token.Sub : {
+//             return OperPres.AddSub
+//         };
+//         case Token.Multi : {
+//             return OperPres.MulDiv
+//         };
+//         case Token.Divide : {
+//             return OperPres.MulDiv
+//         };
+//         case Token.Caret : {
+//             return OperPres.Power
+//         };
+//         case Token.LeftPerm : {
+//             return OperPres.Perm
+//         };
+//         case Token.RightPerm : {
+//             return OperPres.Perm
+//         };
+//         default : {
+//             return OperPres.DefaultZero
+//         }
+//     }
+// }
+
+// type Chars =  Record< TokenValue, string>
+
+type ValidChars = TokenValue | number ;
+type ArthematicToken = Token.Add | Token.Sub | Token.Multi | Token.Divide | Token.Caret
+type ArthematicOpertor =  "add" | "sub" | "multi" | "div" | "power";
+
+type AllOpType = ArthematicOpertor | "plus" | "minus" | "perm" | "num" | "undefined";
+
+type TreeNode={
+    isBinaryOp: false,
+    opType: "plus" | "minus" | "perm" ,
+    value?: number,
+    leftChild: TreeNode,
+} | {
+    isBinaryOp: false,
+    opType: "num",
+    value: number,
+} | {
+    isBinaryOp: false,
+    opType: "undefined",
+} | {
+    isBinaryOp: true,
+    opType: ArthematicOpertor,
+    value?: number,
+    leftChild: TreeNode,
+    rightChild: TreeNode,
+};
+
+type Left = {isLeft: true,leftTree?: TreeNode} | {isLeft: false, leftTree: TreeNode}
+
+type LeftWithPrevOp = Left & {
+    prevOpe?: Token.Add | Token.Sub | Token.Multi | Token.Divide | Token.Caret 
+}
+
+function getOpeTypePrecedence(opType: AllOpType){
+    switch(opType){
+        case "power": return 3;
+        case "multi": return 2;
+        case "div": return 2;
+        case "add": return 1;
+        case "sub": return 1;
+        default: return 0;
+    }   
+}
+
+function add(x: number,y: number){
+    return x+y;
+}
+
+function sub(x: number,y: number){
+    return x-y;
+}
+
+function mult(x: number,y: number){
+    return x*y;
+}
+
+function div(x: number,y: number){
+    return x/y;
+}
+
+function pow(x: number,y: number){
+    return Math.pow(x,y);
+}
+
+function num(x: string | number){
+    return Number(x);
+}
+
+function minus(x: number){
+    return -1*x;
+}
+
+function plus(x: number){
+    return -1*x;
+}
+
+function perm(x: number){
+    return x;
+}
+
+const getFun  = {
+    "add":add,
+    "sub":sub,
+    "multi":mult,
+    "div":div,
+    "power":pow,
+    "num":num,
+    "minus": minus,
+    "plus":plus,
+    "perm":perm,
+}
+
+
+function getParsedTokens(input: string): TokenObj[] | Error{
+    const tokens: TokenObj[]=[];
+    input = input.replace(/\s/g,'');
+    const tokenChars = input.split('') as Array<ValidChars>
+    for(let i=0;i<tokenChars.length;i++){
+        const char= tokenChars[i];
+        let validToken: undefined | Token = undefined;
+        let n= Number(char);
+        if(char && !isNaN(n)){
+            let j=i+1;
+            let nextChar = tokenChars[j];
+            let nextCharNumber = Number(nextChar);
+            while(nextChar && !isNaN(nextCharNumber)){
+                n=n*10 + nextCharNumber;
+                j++
+                i++;
+                nextChar = tokenChars[j];
+                nextCharNumber = Number(nextChar);
+            };
+            validToken = Token.Num;
+            tokens.push({
+                token: validToken,
+                data: n,
+            })
+        }else {
+            switch(char){
+                case TokenValue.Pluse : {
+                    validToken = Token.Add
+                    break;
+                };
+                case TokenValue.Minus : {
+                    validToken = Token.Sub
+                    break;
+                };
+                case TokenValue.Star : {
+                    validToken = Token.Multi
+                    break;   
+                };
+                case TokenValue.ForwardSlash : {
+                    validToken = Token.Divide
+                    break;   
+                };
+                case TokenValue.Caret : {
+                    validToken = Token.Caret
+                    break;   
+                };
+                case TokenValue.LeftPerm : {
+                    validToken = Token.LeftPerm
+                    break;   
+                };
+                case TokenValue.RightPerm : {
+                    validToken = Token.RightPerm
+                    break;   
+                };
+                default: {
+                    console.log("invalid char ",char)
+                    return Error("Invalid expression")
+                }
+            }
+            if(validToken){
+                tokens.push({
+                    token: validToken as Exclude<Token, Token.Num>,
+                })
+            }
+        }
+
+    }
+    return tokens;
+}
+
+function getInnerTokens(tokenObjs: TokenObj[]){
+    let internalTokens: TokenObj[]=[];
+    let pt= tokenObjs.shift();
+    let no_of_left = 0;
+    while(pt && (pt.token != Token.RightPerm || no_of_left>0)){
+        internalTokens.push(pt);
+        if(pt.token == Token.LeftPerm){
+            no_of_left++;
+        }else if(pt.token == Token.RightPerm){
+            no_of_left--;
+        }
+        pt = tokenObjs.shift();
+    }
+    return internalTokens;
+}
+
+function getArthematicOpertor(token:ArthematicToken):ArthematicOpertor{
+    switch(token){
+        case Token.Add : {
+            return "add"
+        };
+        case Token.Sub : {
+            return "sub"
+        };
+        case Token.Multi : {
+            return 'multi'
+        };
+        case Token.Divide : {
+            return "div"
+        };
+        case Token.Caret : {
+            return "power"
+        };
+    }
+}
+
+function getAbstractTree(tokenObjs: TokenObj[], left : LeftWithPrevOp = { isLeft: true }  ): TreeNode{
+    let tokenObj= tokenObjs.shift();
+    // let len = tokenObjs.length;
+    const {isLeft, prevOpe } = left;
+    if(!tokenObj){
+        return {
+            isBinaryOp: false,
+            opType: "undefined",
+        }
+    }
+    if(tokenObj.token == Token.LeftPerm){
+
+        const internalTokens: TokenObj[]= getInnerTokens(tokenObjs)
+        const ast: TreeNode = getAbstractTree(internalTokens);
+        const currentTreeNode: TreeNode ={
+            isBinaryOp: false,
+            opType: "perm",
+            leftChild: ast,
+        }
+
+        if(tokenObjs.length>0){
+            return getAbstractTree(tokenObjs,{ isLeft: false, leftTree:  currentTreeNode})
+        }
+
+        return currentTreeNode;
+    }
+
+    if(isLeft){
+        if(tokenObj.token == Token.Num){
+            const currentNode: TreeNode = {
+                isBinaryOp: false,
+                opType: "num",
+                value: tokenObj.data
+            }
+            if(tokenObjs.length){
+                return getAbstractTree(tokenObjs, { isLeft: false, leftTree:  currentNode});
+            }
+            return currentNode;
+        }else if(tokenObj.token == Token.Add || tokenObj.token == Token.Sub){
+            const nextTokenObj = tokenObjs.shift();
+            if(nextTokenObj){
+                if(nextTokenObj.token == Token.LeftPerm){
+                    const internalTokens: TokenObj[]= getInnerTokens(tokenObjs)
+                    const ast = getAbstractTree(internalTokens);
+                    const currentTreeNode: TreeNode ={
+                        isBinaryOp: false,
+                        opType: tokenObj.token == Token.Add ? "plus" : "minus",
+                        leftChild: ast
+                    }
+                    if(tokenObjs.length>0){
+                        return getAbstractTree(tokenObjs,{ isLeft: false, leftTree:  currentTreeNode})
+                    }
+                    return currentTreeNode;
+                }else if(nextTokenObj.token == Token.Num){
+                    const numberNode: TreeNode = {
+                        isBinaryOp: false,
+                        opType: "num",
+                        value: nextTokenObj.data
+                    }
+                    const currentTreeNode: TreeNode ={
+                        isBinaryOp: false,
+                        opType: tokenObj.token == Token.Add ? "plus" : "minus",
+                        leftChild: numberNode
+                    }
+                                
+                    if(tokenObjs.length>0){
+                        return getAbstractTree(tokenObjs,{ isLeft: false, leftTree:  currentTreeNode})
+                    }
+                    return currentTreeNode;
+                    }
+            }
+        }
+        return {
+            isBinaryOp: false,
+            opType: "undefined",
+        }
+    }else{
+        const token = tokenObj.token;
+        if(Token.Add == token || Token.Sub == token || Token.Divide == token || Token.Multi == token || Token.Caret == token ){
+            // const nextTokenObj = tokenObjs[0];
+            const rightTree = getAbstractTree(tokenObjs);
+            // if(rightTree)
+            // const rightTreeToken = rightTree.opType;
+            const opType = getArthematicOpertor(token);
+            const presentTokenPrec = getOpeTypePrecedence(opType)
+            const childTokenPrec = getOpeTypePrecedence(rightTree.opType)
+            if(childTokenPrec == 0 || presentTokenPrec <= childTokenPrec)
+                return {
+                    isBinaryOp: true,
+                    opType: opType ,
+                    leftChild: left.leftTree,
+                    rightChild: rightTree,
+                }
+            else{
+                if(rightTree.isBinaryOp){
+                    const currentTreeNode: TreeNode ={
+                        isBinaryOp: true,
+                        opType: opType ,
+                        leftChild: left.leftTree,
+                        rightChild: rightTree.leftChild 
+                    }
+                    rightTree.leftChild= currentTreeNode
+                }
+                return rightTree;
+                
+            }
+        }
+        return {
+            isBinaryOp: false,
+            opType: "undefined",
+        }
+    }
+}
+
+function getValueFromAST(t: TreeNode) : number{
+    if(t.opType=="num"){
+        return t.value;
+    }
+    if(t.opType == "undefined"){
+        return 0;
+    }
+
+    if(t.isBinaryOp==true){
+        const x=getValueFromAST(t.leftChild);
+        const y=getValueFromAST(t.rightChild);
+        const f=getFun[t.opType];
+        return f(x,y);
+    }else{
+        const x=getValueFromAST(t.leftChild);
+        const f=getFun[t.opType];
+        return f(x);
+    }
+}
+
+export function evaluate(expression: string){
+    if(typeof(expression) !== "string"){
+        return Error("Invalid expression type")
+    }
+    const tokensOrError = getParsedTokens(expression);
+    if(tokensOrError instanceof Error){
+        return tokensOrError;
+    }
+    if(tokensOrError.length == 0){
+        return NaN
+    }
+    const ast = getAbstractTree(tokensOrError);
+    const value = getValueFromAST(ast);
+    return value
+}
